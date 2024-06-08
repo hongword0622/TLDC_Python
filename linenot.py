fdb57cb7ab4ec68641d561556b59fcbeeeaa1685
hongword0622 <120542124+hongword0622@users.noreply.github.com>
2024-06-08 12:01:33 +0800

    Update linenot.py

--- linenot.py
+++ linenot.py
@@ -3,7 +3,8 @@ from pymysql import OperationalError
 import requests
 from datetime import datetime
 import time
-# MySQL 連線設定
+
+# MySQL Config設定
 connection_pool = pymysql.connect(
     host="10.8.1.3",
     user="root",
@@ -13,27 +14,31 @@ connection_pool = pymysql.connect(
     autocommit=True
 )
 # Line Notify 設定  
-#line_notify_token = '2qQr0Bh9Pbfl6MyMujxK7HjS2KftJal5qJegbF9Klae'
 line_notify_token = 'SVBeAIKTJECrGSU8m7NeFhczlBiI43yyDjMOPY3NIcI'
 line_notify_api = 'https://notify-api.line.me/api/notify'
-#KM_state, KM_cinema_state, Taipei_state = False, False, False
 
 def Send_Check():
+    # 全域變數
     global KM_state, KM_cinema_state, Taipei_state
+
+    # 建立資料庫連線
     cursor1 = connection_pool.cursor()
 
-    # 獲取所有已啟用的 location
+    # 取得所有啟用的 location資料
     locations_query = "SELECT DISTINCT Location FROM temp_humd_info WHERE Enable_Status = 'ON'"
     cursor1.execute(locations_query)
     country_result = cursor1.fetchall()
     print(f"Enabled locations: {country_result}")
 
+    # 初始化狀態列表
     KM_state = []
     KM_cinema_state = []
     Taipei_state = []
 
+    # 建立資料庫連線
     cursor = connection_pool.cursor()
 
+    # 定義查詢語句以取得最新的狀態資料
     send_check_query = """
     SELECT state, Location, Type, MAX(Time) AS max_time
     FROM (
@@ -44,19 +49,26 @@ def Send_Check():
     ) AS RankedRecords
     GROUP BY Location, Type;
     """
+
+    # 執行查詢語句
     cursor.execute(send_check_query)
     locations_result = cursor.fetchall()
 
+    # 逐一查詢結果
     for location_data in locations_result:
-        #print(location_data)
 
+        # 分割欄位資料
         state, location, data_type, Dates = location_data
 
+        # 檢查狀態是否為 "1"
         if state == "1":
+            # 狀態為 "1" 時，檢查溫濕度
             check_temperature_humidity(location, data_type)
         else:
+            # 狀態不為 "1" 時，顯示 None
             print("None")
 
+#發送LINE通知
 def send_line_notify(message, token):
     url = "https://notify-api.line.me/api/notify"
     headers = {
@@ -68,20 +80,19 @@ def send_line_notify(message, token):
     response = requests.post(url, headers=headers, params=payload)
     return response.status_code
 
+#檢查溫溼度是否超過標準
 def check_temperature_humidity(location, data_type):
     try:
+    #建立資料庫連線
         cursor = connection_pool.cursor()
-
+    #查詢所有地點
         locations_query = "SELECT DISTINCT Location FROM temp_humd_info WHERE country = %s"
         cursor.execute(locations_query, (location,))
+    #取出地點資料並將資料進行逗點分隔處理
         locations_result = cursor.fetchall()
-
-        # 檢查是否獲取到有效的 location
-        #print(f"Locations for {location}: {locations_result}")
-
         location_list = [f"'{row[0]}'" for row in locations_result]
         placeholders = ', '.join(location_list)
-
+    #查詢各地點已啟用最新溫溼度資料
         temp_query = """
         SELECT t1.type, t1.value, t1.location, t1.datetime
         FROM t1
@@ -99,12 +110,13 @@ def check_temperature_humidity(location, data_type):
         """.format(placeholders)
 
         cursor.execute(temp_query, (location, data_type))
+        #取得各地點溫溼度查詢資料
         temp_results = cursor.fetchall()
-        #print(f"Results for {location} ({data_type}): {temp_results}")
 
         temperature_data = {}
         humidity_data = {}
 
+        #將溫溼度進行分類，並將觀測值與時間分開儲存
         for row in temp_results:
             data_type, value, location, timestamp = row
 
@@ -121,23 +133,26 @@ def check_temperature_humidity(location, data_type):
                 humidity_data[location]['timestamp'].append(timestamp)
 
         cursor2 = connection_pool.cursor()
-
+        
+#查詢溫溼度標準
         std_query = "SELECT Temp_Min, Temp_Max, Humd_Min, Humd_Max FROM temp_humd_stardard ORDER BY id desc limit 1;"
         cursor2.execute(std_query)
         temphund_std = cursor2.fetchone()
-
+        
+#確認是否取得標準
         if temphund_std:
             lt, gt, lh, gh = temphund_std
-
+            
+# 設定與轉換溫度濕度標準
         temperature_threshold = int(gt)
         humidity_threshold = int(gh)
         temperature_lthreshold = int(lt)
         humidity_lthreshold = int(lh)
 
+ # 準備與對應所有地點的溫度和濕度資料
         location_list = [row[0].strip("'") for row in locations_result]
         all_temp_data = []
         all_humd_data = []
-
         for loc in location_list:
             if loc in temperature_data:
                 all_temp_data.extend(temperature_data[loc]['value'])
@@ -145,11 +160,9 @@ def check_temperature_humidity(location, data_type):
             if loc in humidity_data:
                 all_humd_data.extend(humidity_data[loc]['value'])
 
-        #print("All Temperature Data:", all_temp_data)
-        #print("All Humidity Data:", all_humd_data)
-
         # 檢查是否超過溫度或濕度閾值，並發送 LINE Notify
         if any(temp > temperature_threshold for temp in all_temp_data):
+            # 所在地點設備溫度高於閾值
             exceeded_temp_locations = [loc for loc in location_list if any(temp > temperature_threshold for temp in temperature_data[loc]['value'])]
             exceeded_values_str = ', '.join(map(str, [temp for temp in all_temp_data if temp > temperature_threshold]))
             huhs = f"\n目前溫度 : {exceeded_values_str}˚C\n地點 : {', '.join(exceeded_temp_locations)}\n狀態 : 已超過標準{temperature_threshold}˚C!!!"
@@ -157,6 +170,7 @@ def check_temperature_humidity(location, data_type):
             print("溫度Over", huhs)
 
         elif any(temp < temperature_lthreshold for temp in all_temp_data):
+            # 所在地點設備溫度低於標準
             exceeded_temp_locations = [loc for loc in location_list if any(temp < temperature_lthreshold for temp in temperature_data[loc]['value'])]
             exceeded_values_str = ', '.join(map(str, [temp for temp in all_temp_data if temp < temperature_lthreshold]))
             huhs = f"\n目前溫度 : {exceeded_values_str}˚C\n地點 : {', '.join(exceeded_temp_locations)}\n狀態 : 已低於標準{temperature_lthreshold}˚C!!!"
@@ -164,6 +178,7 @@ def check_temperature_humidity(location, data_type):
             print("溫度Down", huhs)
 
         if any(humd > humidity_threshold for humd in all_humd_data):
+            # 所在地點設備濕度高於標準
             exceeded_humd_locations = [loc for loc in location_list if any(humd > humidity_threshold for humd in humidity_data[loc]['value'])]
             exceeded_values_str = ', '.join(map(str, [humd for humd in all_humd_data if humd > humidity_threshold]))
             huhs = f"\n目前濕度 : {exceeded_values_str}%\n地點 : {', '.join(exceeded_humd_locations)}\n狀態 : 已超過標準{humidity_threshold}%!!!"
@@ -171,17 +186,17 @@ def check_temperature_humidity(location, data_type):
             print("濕度Over", huhs)
 
         elif any(humd < humidity_lthreshold for humd in all_humd_data):
+            # 所在地點設備濕度低於標準
             exceeded_humd_locations = [loc for loc in location_list if any(humd < humidity_lthreshold for humd in humidity_data[loc]['value'])]
             exceeded_values_str = ', '.join(map(str, [humd for humd in all_humd_data if humd < humidity_lthreshold]))
             huhs = f"\n目前濕度 : {exceeded_values_str}%\n地點 : {', '.join(exceeded_humd_locations)}\n狀態 : 已低於標準{humidity_lthreshold}%!!!"
             send_line_notify(huhs, line_notify_token)
             print("濕度Down", huhs)
+#例外狀況
     except Exception as e:
         print(f"Error: {e}")
 
-# 呼叫檢查函式
+# 主程式執行
 Send_Check()
-#time.sleep(60)
+#由於目前警報是每小時進行偵測與通知，上述程式為一次性，現透過排程以每小時進行
 
-#https://10-214-0-55.tldc-hl-nas3.direct.quickconnect.to:5001/#
-#https://10.214.0.55:5001/
